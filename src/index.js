c282967887d1ed3a1e5902e891f93403dc12c220
nmaynes <nmaynes@pbs.org>
2022-10-27 11:26:49 -0400

    Add port listener config in proper position

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 const TrackAPI = require('./datasources/track-api');
 
-await server.listen({port: process.env.PORT || 4000});
+
 
 async function startApolloServer(typeDefs, resolvers) {
   const server = new ApolloServer({
@@ -16,7 +16,7 @@ async function startApolloServer(typeDefs, resolvers) {
     },
   });
 
-  const { url, port } = await server.listen();
+  const { url, port } = await server.listen({port: process.env.PORT || 4000});
   console.log(`
       🚀  Server is running
       🔉  Listening on port ${port}
